d2aae637412c6e567de62fd7aaf54b44bc09ea5d
yasumiya <yasto313@gmail.com>
2025-05-08 14:25:58 +0900

    仮プロトタイプ

---
+++ My project (3)/Assets/Script/CameraManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public float targetDistanceX = 0f;
+    public float targetDistanceY = 3f;
+    public float targetDistanceZ = -3f;
+    public float smoothSpeed = 5f;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 targetPos = player.transform.position + new Vector3(targetDistanceX, targetDistanceY, targetDistanceZ);
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothSpeed * Time.deltaTime);
+        }
+    }
+}
